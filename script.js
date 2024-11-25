e6cc59d90b38c3eb1cfe40ecb4647ff864e1feed
박세진 <128587812+imnotidiot@users.noreply.github.com>
2024-11-25 12:48:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-const startButton = document.getElementById('startButton');
-const difficultyMenu = document.getElementById('difficultyMenu');
-const difficultyButtons = document.querySelectorAll('.difficulty-button');
 
-startButton.addEventListener('click', () => {
-    difficultyMenu.classList.remove('hidden');
-});
-
-difficultyButtons.forEach(button => {
-    button.addEventListener('click', (event) => {
-        const selectedDifficulty = event.target.getAttribute('data-difficulty');
-        alert(`Starting game with difficulty: ${selectedDifficulty}`);
-        startGame(selectedDifficulty);
-    });
-});
-
-function startGame(difficulty) {
-    window.location.href = "2.html";
-}
